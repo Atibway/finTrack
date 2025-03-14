e5491af095f5bf2798edbba7b4aeb6e9400744cf
Atibway <ainebyoonaatiidu@gmail.com>
2025-03-14 16:19:49 +0300

    ok

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,33 +1,31 @@
-
 import Sidebar from "@/components/layout/Sidebar";
 import TopBar from "@/components/layout/TopBar";
 import { currentUser } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
-type Props ={
-    children: React.ReactNode;
-}
-const layout = async({
-children
-}: Props) => {
-    const isLoggedIn = await currentUser()
-  if(!isLoggedIn){
-    redirect("/login")
+type Props = {
+  children: React.ReactNode;
+};
+
+const layout = async ({ children }: Props) => {
+  const isLoggedIn = await currentUser();
+  if (!isLoggedIn) {
+    redirect("/login");
   }
 
   return (
     <>
-    <div className="min-h-screen  flex ">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <TopBar />
-        <main className="flex-1 p-3 ">
-          {children}
-        </main>
+      <div className="min-h-screen flex">
+        <Sidebar />
+        <div className="flex-1 flex flex-col ml-[250px]"> {/* Adjust the margin-left to match the sidebar width */}
+          <TopBar />
+          <main className="flex-1 p-3">
+            {children}
+          </main>
+        </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default layout
+export default layout;
