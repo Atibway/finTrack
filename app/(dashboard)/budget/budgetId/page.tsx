1a1d5a498d8034b61c7d3f3e81dd0be735b856a0
Atibway <ainebyoonaatiidu@gmail.com>
2025-03-14 12:02:22 +0300

    ok

---
+++ app/(dashboard)/budget/budgetId/page.tsx
@@ -0,0 +1,17 @@
+// import { BudgetDetailComponent } from "./_components/budgetIdComponent"
+
+
+const page = async () => {
+  return (
+    <>
+    {/* <BudgetDetailComponent 
+    id={params.budgetId} */}
+    {/* /> */}
+    <div>
+      hello
+    </div>
+    </>
+  )
+}
+
+export default page
